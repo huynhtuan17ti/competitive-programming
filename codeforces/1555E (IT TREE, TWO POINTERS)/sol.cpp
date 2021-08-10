99e0b06e742ef9852dd06e740b525866bf36b6f2
huynhtuan17ti <huynhminhtuan6429@gmail.com>
2021-08-10 23:48:09 +0700

    add 1555E

--- codeforces/1555E (IT TREE, TWO POINTERS)/sol.cpp
+++ codeforces/1555E (IT TREE, TWO POINTERS)/sol.cpp
@@ -12,48 +12,72 @@ typedef vector<ii> vii;
 #define se second
 #define io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
  
-const int MAXN = 1e6+5;
-int m;
+template <typename T, typename F = function <T(const T&, const T&)>>
+class LazySegmentTree{
+    F func;
+    T blank;
+    struct node{
+        T val, lazy;
+        node(){
+            val = lazy = 0;
+        }
+    };
+    public:
+        vector <node> IT;
+        LazySegmentTree(int n, T blank, const F& f){
+            IT.resize(4*n+5);
+            this->func = f;
+            this->blank = blank;
+        }
+        void push(int id, T val){
+            IT[id].val += val;
+            IT[id].lazy += val;
+        }
+        void down(int id){
+            T k = IT[id].lazy;
+            if(k == blank)
+                return;
+
+            push(2*id, k);
+            push(2*id+1, k);
+
+            IT[id].lazy = blank;
+        }
+        void update(int id, int l, int r, int u, int v, T val){
+            if(v < l || r < u)
+                return;
+            if(u <= l && r <= v){
+                push(id, val);
+                return;
+            }
+            down(id);
+            int mid = (l+r) >> 1;
+            update(2*id, l, mid, u, v, val);
+            update(2*id+1, mid+1, r, u, v, val);
+            IT[id].val = func(IT[2*id].val, IT[2*id+1].val);
+        }
+        T get(int id, int l, int r, int u, int v){
+            if(v < l || r < u)
+                return blank;
+            if(u <= l && r <= v)
+                return IT[id].val;
+            down(id);
+            int mid = (l+r) >> 1;
+            T L = get(2*id, l, mid, u, v);
+            T R = get(2*id+1, mid+1, r, u, v);
+            return func(L, R);
+        }
+};
 
 struct seg{
     int l, r, w;
 };
 
-int IT[4*MAXN], lazy[4*MAXN];
-
-void down(int id){
-    int k = lazy[id];
-    if(k == 0)
-        return;
-    IT[2*id] += k;
-    IT[2*id+1] += k;
-
-    lazy[2*id] += k;
-    lazy[2*id+1] += k;
-
-    lazy[id] = 0;
-}
-
-void update(int id, int l, int r, int u, int v, int val){
-    if(v < l || r < u)
-        return;
-    if(u <= l && r <= v){
-        IT[id] += val;
-        lazy[id] += val;
-        return;
-    }
-    int mid = (l+r) >> 1;
-    down(id);
-    update(2*id, l, mid, u, v, val);
-    update(2*id+1, mid+1, r, u, v, val);
-    IT[id] = min(IT[2*id], IT[2*id+1]);
-}
-
 int main(){
     io
     int t; t = 1;
     while(t--){
-        int n;
+        int n, m;
         cin >> n >> m;
         vector <seg> segs;
         for(int i = 0; i < n; i++){
@@ -65,19 +89,23 @@ int main(){
         sort(segs.begin(), segs.end(), [](const seg &A, const seg &B){
             return (A.w < B.w);
         });
+
+        LazySegmentTree <int> ST(m, 0, [&](int i, int j){
+            return min(i, j);
+        });
         int j = 0;
         int res = INT_MAX;
         for(int i = 0; i < segs.size(); i++){
-            update(1, 1, m-1, segs[i].l, segs[i].r, 1);
-            while(j < i && IT[1] > 0){
-                update(1, 1, m-1, segs[j].l, segs[j].r, -1);
+            ST.update(1, 1, m-1, segs[i].l, segs[i].r, 1);
+            while(j < i && ST.IT[1].val > 0){
+                ST.update(1, 1, m-1, segs[j].l, segs[j].r, -1);
                 j += 1;
             }
-            if(j > 0 && IT[1] == 0){
+            if(j > 0 && ST.IT[0].val == 0){
                 j -= 1;
-                update(1, 1, m-1, segs[j].l, segs[j].r, 1);
+                ST.update(1, 1, m-1, segs[j].l, segs[j].r, 1);
             }
-            if(IT[1] > 0){
+            if(ST.IT[1].val > 0){
                 res = min(res, segs[i].w - segs[j].w);
             }
         }
