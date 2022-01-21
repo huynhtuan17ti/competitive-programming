0954924da4d8393641f630fae79ca5d54805e84d
huynhtuan17ti <huynhminhtuan6429@gmail.com>
2022-01-22 00:15:49 +0700

    add 1695

---
+++ online_judge/cses/1695/sol.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>  
+using namespace std;
+
+const int MOD = 1e9 + 7;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+#define mp make_pair
+#define fi first
+#define se second
+#define io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+const int MAXN = 505;
+const ll inf = 1e13;
+
+vector <int> G[MAXN];
+ll c[MAXN][MAXN];
+ll f[MAXN][MAXN];
+ll len[MAXN]; int trace[MAXN], Set[MAXN];
+
+bool short_paths(int s, int e, int n){
+    fill(len+1, len+1+n, inf);
+    fill(Set+1, Set+1+n, 2);
+    deque <int> Q;
+    Q.push_back(s); len[s] = 0;
+    while(!Q.empty()){
+        int u = Q.front();
+        Q.pop_front();
+        Set[u] = 0;
+        for(int v: G[u]){
+            ll cost = (f[u][v] >= 0)? 1 : -1;
+            if(c[u][v] > f[u][v] && len[v] > len[u] + cost){
+                len[v] = len[u] + cost;
+                trace[v] = u;
+                if(Set[v] == 0){
+                    Set[v] = 1;
+                    Q.push_front(v);
+                }
+                if(Set[v] == 2){
+                    Set[v] = 1;
+                    Q.push_back(v);
+                }
+            }
+        }
+    }
+    return (len[e] != inf);
+}
+
+bool vi[MAXN];
+
+void DFS(int u){
+    vi[u] = 1;
+    for(int v: G[u])
+        if(f[u][v] <= 0 && !vi[v])
+            DFS(v);
+}
+
+int main(){
+    io
+    // freopen("test.inp", "r", stdin);
+    // freopen("test.out", "w", stdout);
+    int n, m;
+    cin >> n >> m;
+    vii edges;
+    while(m--){
+        int u, v;
+        cin >> u >> v;
+        edges.push_back({u, v});
+        G[u].push_back(v);
+        G[v].push_back(u);
+        c[u][v] = c[v][u] = 1;
+    }
+
+    ll max_flow = 0;
+    int s = 1, e = n;
+    while(short_paths(1, n, n)){
+        ll flow = inf;
+        for(int v = e; v != s; v  = trace[v]){
+            int u = trace[v];
+            flow = min(flow, c[u][v] - f[u][v]);
+        }
+        for(int v = e; v != s; v  = trace[v]){
+            int u = trace[v];
+            f[u][v] += flow;
+            f[v][u] -= flow;
+        }
+        max_flow += flow;
+    }
+    cout << max_flow << '\n';
+    DFS(1);
+    for(auto [u, v]: edges){
+        //cerr << u << ' ' << v << ' ' << f[u][v] << '\n';
+        //cerr << v << ' ' << u << ' ' << f[v][u] << '\n';
+        if(f[u][v] > 0 && vi[u] && !vi[v])
+            cout << u << ' ' << v << '\n';
+        if(f[v][u] > 0 && vi[v] && !vi[u])
+            cout << v << ' ' << u << '\n';
+    }
+}
