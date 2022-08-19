8a53ba91754d932fe50c9909506f30b5559a27d4
huynhtuan17ti <huynhminhtuan6429@gmail.com>
2022-08-19 22:49:57 +0700

    add 1136

---
+++ online_judge/cses/1136/sol.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>  
+using namespace std;
+
+const int MOD = 1e9 + 7;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+#define mp make_pair
+#define fi first
+#define se second
+#define io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+const int MAXN = 5e5 + 5;
+
+vector <int> G[MAXN];
+
+class LCA{
+public:
+    vector <int> h;
+    vector <vector <int>> R;
+
+    LCA(int n){
+        h.resize(n+1);
+        R.resize(n+1);
+        int sz = trunc(log2(n)+1);
+        for(int i = 0; i <= n; i++)
+            R[i].resize(sz);
+        DFS_level(1, 0);
+        for(int j = 1; j <= log2(n); j++)
+            for(int i = 1; i <= n; i++)
+                R[i][j] = R[R[i][j-1]][j-1];
+    }
+
+    void DFS_level(int u, int pre){
+        for(int v: G[u])
+            if(v != pre){
+                h[v] = h[u] + 1;
+                R[v][0] = u;
+                DFS_level(v, u);
+            }
+    }
+
+    int lca(int u, int v){
+        if(h[u] < h[v])
+            swap(u, v);
+        
+        for(int i = log2(h[u]); i >= 0; i--)
+            if(h[u] - (1<<i) >= h[v])
+                u = R[u][i];
+        
+        if(u == v)
+            return u;
+
+        for(int i = log2(h[u]); i >= 0; i--)
+            if(R[u][i] != 0 && R[u][i] != R[v][i]){
+                u = R[u][i];
+                v = R[v][i];
+            }
+        return R[u][0];
+    }
+
+    // get distance between (u, v)
+    int get(int u, int v){
+        int p = lca(u, v);
+        return h[u] + h[v] - h[p]*2;
+    }
+};
+
+int sum[MAXN];
+
+void DFS(int u, int pre){
+    for(int v: G[u])
+        if(v != pre){
+            DFS(v, u);
+            sum[u] += sum[v];
+        }
+}
+
+int main(){
+    io
+    // freopen("test.inp", "r", stdin);
+    // freopen("test.out", "w", stdout);
+    int n, m;
+    cin >> n >> m;
+    for(int i = 1; i < n; i++){
+        int u, v;
+        cin >> u >> v;
+        G[u].push_back(v);
+        G[v].push_back(u);
+    }
+    LCA lca(n);   
+    while(m--){
+        int u, v;
+        cin >> u >> v;
+        int p = lca.lca(u, v);
+        sum[u] += 1;
+        sum[v] += 1;
+        sum[p] -= 1;
+        if(p != 1)
+            sum[lca.R[p][0]] -= 1;
+    }
+    DFS(1, 0);
+    for(int u = 1; u <= n; u++)
+        cout << sum[u] << " \n"[u == n];
+}
--- online_judge/cses/README.md
+++ online_judge/cses/README.md
@@ -130,3 +130,4 @@
 | [1687 - Company Queries I](1687)          | `Graph`, `DFS`, `Tree`, `Sparse Table` |
 | [1688 - Company Queries II](1688)         | `Graph`, `DFS`, `Tree`, `LCA` |
 | [1135 - Distance Queries](1135)           | `Graph`, `DFS`, `Tree`, `LCA` |
+| [1136 - Counting Paths](1136)             | `Graph`, `DFS`, `Tree`, `LCA`, `Prefix Sum` |
