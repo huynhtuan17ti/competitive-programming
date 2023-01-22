a49e9c22d1a1c076b3b074879d040a633c8cdd1d
huynhtuan17ti <huynhminhtuan6429@gmail.com>
2023-01-22 21:35:32 +0700

    fix template

--- template/MatrixMulti.h
+++ template/MatrixMulti.h
@@ -11,19 +11,19 @@ struct Matrix{
 		data.resize(dim);
 		for(int i = 0; i < dim; ++i)
 		    data[i].resize(dim, 0);
-			if(identity)
-				for(int i = 0; i < dim; ++i) data[i][i] = 1;
+		if(identity)
+			for(int i = 0; i < dim; ++i) data[i][i] = 1;
 	 }
 
 	Matrix operator * (const Matrix<T> &other) {
 		Matrix<T> res;
 		for(int k = 0; k < dim; ++k)
 			for(int i = 0; i < dim; ++i)
-		    		for(int j = 0; j < dim; ++j) {
-		        		res.data[i][j] += data[i][k] * other.data[k][j] % MOD;
-					res.data[i][j] %= MOD;
-				}
-	    	return res;
+		    	for(int j = 0; j < dim; ++j) {
+		        	res.data[i][j] += data[i][k] * other.data[k][j] % MOD;
+                    res.data[i][j] %= MOD;
+                }
+	    return res;
 	}
 
 	// fast calculating A^b
